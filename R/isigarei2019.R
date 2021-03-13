9874a566b3a2ecf32a5e9e4069b2d4f74cfd5b4c
Yuki-Kanamori <y.kanamory@gmail.com>
2021-03-13 16:29:06 +0900

    initial commit

---
+++ R/isigarei2019.R
@@ -0,0 +1,400 @@
+setwd("/Users/Yuki/Dropbox/eDNA_INLA")
+
+require(INLA)
+require(tidyverse)
+require(openxlsx)
+
+# 2019 ----------------------------------------------------------
+# eDNA & env ----------------------------------------------------------
+mifish = read.csv("joint_edna2019.csv")
+
+# sp1 = read.xlsx("splist2019.xlsx", sheet = 1, colNames = T)
+# sp2 = read.xlsx("splist2019.xlsx", sheet = 2, colNames = T)
+
+# catch ---------------------------------------------------------
+# データは1-12月
+data = read.csv("joint_cpue2019.csv")
+
+
+# スズキ ------------------------------------
+e_fish = mifish %>% filter(sp_group == "isigarei", SorB == "B")
+summary(e_fish)
+unique(e_fish$ID)
+edna = (e_fish$copy > 0) + 0
+
+c_fish = data %>% filter(sp == "isigarei")
+summary(c_fish)
+catch = (c_fish$catch > 0) + 0
+summary(catch)
+
+# e_fish = e_fish %>% mutate(Month = as.factor(month))
+# c_fish = c_fish %>% mutate(Month = as.factor(M), Gear = as.factor(GEAR))
+
+# INLA ----------------------------------------------------------
+#lonlat
+e_loc = as.matrix(cbind(e_fish$lng, e_fish$lat))
+c_loc = as.matrix(cbind(c_fish$lon, c_fish$lat))
+loc = rbind(e_loc, c_loc)
+
+c_locp = as.matrix(cbind(c_fish %>% filter(catch > 0) %>% select(lon), c_fish %>% filter(catch > 0) %>% select(lat)))
+c_loca = as.matrix(cbind(c_fish %>% filter(catch == 0) %>% select(lon), c_fish %>% filter(catch == 0) %>% select(lat)))
+
+bound2 = inla.nonconvex.hull(loc, convex = 0.05, concave = -0.15)
+mesh2 = inla.mesh.2d(boundary = bound2, max.edge = c(0.04, 0.04), cutoff = 0.08/5)
+plot(mesh2)
+points(c_locp, col = "red", pch = 15, cex = 2) #pdfにする時はcex = 1，その他は.5にする
+points(c_loca, col = "orange", pch = 16, cex = 1) #pdfにする時はcex = 1，その他は.5にする
+points(e_loc, col = "green", pch = 16, cex = 1)
+mesh2$n #651
+
+# projector matricies
+e_A = inla.spde.make.A(mesh2, loc = e_loc)
+dim(e_A) # 246, 651
+table(rowSums(e_A > 0))
+table(rowSums(e_A))
+table(colSums(e_A) > 0)
+c_A = inla.spde.make.A(mesh2, loc = c_loc)
+dim(c_A) # 932, 651
+table(rowSums(c_A > 0))
+table(rowSums(c_A))
+table(colSums(c_A) > 0)
+
+# for prediction ------------------------------------------------
+setwd('/Users/Yuki/FRA/INLAren/spde-book-files')
+
+## ----opts, echo = FALSE, results = 'hide', message = FALSE, warning = FALSE----
+source('R/initial_setup.R')
+opts_chunk$set(
+  fig.path = 'figs/barrier-'
+)
+library(scales)
+library(rgeos)
+## High resolution maps when using map()
+library(mapdata) 
+## Map features, map2SpatialPolygons()
+library(maptools)
+
+
+# Tokyo Bay ---------------------------------------------------------------
+# Select region 
+map <- map("world", "Japan", fill = TRUE,
+           col = "transparent", plot = TRUE)
+IDs <- sapply(strsplit(map$names, ":"), function(x) x[1])
+map.sp <- map2SpatialPolygons(
+  map, IDs = IDs,
+  proj4string = CRS("+proj=longlat +datum=WGS84")) #緯度経度データ
+summary(map.sp)
+
+# make a polygon ------------------------------------------------------------
+pl.sel <- SpatialPolygons(list(Polygons(list(Polygon(
+  cbind(c(139.7, 139.5, 139.7, 140.1, 140.3, 139.9), # x-axis 
+        c(35.1,  35.4,  35.8,  35.8,  35.4,  35.1)), # y-axis
+  FALSE)), '0')), proj4string = CRS(proj4string(map.sp))) #緯度経度データ
+# 35.2->35に変更->35.1に変更
+summary(pl.sel)
+poly.water <- gDifference(pl.sel, map.sp)
+plot(pl.sel)
+plot(map.sp)
+plot(poly.water)
+
+tok_bor = poly.water@polygons[[1]]@Polygons[[1]]@coords
+
+bb_tok = poly.water@bbox
+x = seq(bb_tok[1, "min"] - 1, bb_tok[1, "max"] + 1, length.out = 150)
+y = seq(bb_tok[2, "min"] - 1, bb_tok[2, "max"] + 1, length.out = 150)
+coop = as.matrix(expand.grid(x, y))
+ind = point.in.polygon(coop[, 1], coop[, 2],
+                       tok_bor[, 1], tok_bor[, 2])
+coop = coop[which(ind == 1), ]
+plot(coop, asp = 1)
+
+Ap = inla.spde.make.A(mesh = mesh2, loc = coop)
+dim(Ap) #431, 651
+
+# spde
+spde = inla.spde2.pcmatern(mesh = mesh2, alpha = 2, prior.range = c(0.01, 0.05), prior.sigma = c(1, 0.01))
+
+
+# stack ---------------------------------------------------------
+#eDNA
+# e_stk = inla.stack(data = list(y = cbind(edna, NA)),
+#                    A = list(e_A, 1),
+#                    effects = list(list(i.e = 1:mesh2$n, i.e2 = 1:mesh2$n), list(eb.0 = rep(1, length(edna)), temp = e_fish$temp, salinity = e_fish$salinity, DO = e_fish$DO_mg, pH = e_fish$pH)),
+#                    tag = "e_dat")
+# na = as.matrix(cbind(rep(NA, nrow(coop)), rep(NA, nrow(coop))))
+# ep_stk = inla.stack(data = list(y = cbind(na[, 1], na[, 2])),
+#                     A = list(Ap, 1),
+#                     effects = list(list(i.e = 1:mesh2$n, i.e2 = 1:mesh2$n), 
+#                                    list(eb.0 = rep(1, nrow(coop)), temp = rep(1, nrow(coop)), salinity = rep(1, nrow(coop)), DO = rep(1, nrow(coop)), pH = rep(1, nrow(coop)))
+#                     ),
+#                     tag = "ep_dat")
+# stk_edna = inla.stack(e_stk, ep_stk)
+
+e_stk = inla.stack(data = list(y = cbind(edna, NA)),
+                   A = list(e_A, 1),
+                   effects = list(list(i.e = 1:mesh2$n, i.e2 = 1:mesh2$n), list(eb.0 = rep(1, length(edna)), temp = e_fish$temp, salinity = e_fish$salinity, pH = e_fish$pH)),
+                   tag = "e_dat")
+na = as.matrix(cbind(rep(NA, nrow(coop)), rep(NA, nrow(coop))))
+ep_stk = inla.stack(data = list(y = cbind(na[, 1], na[, 2])),
+                    A = list(Ap, 1),
+                    effects = list(list(i.e = 1:mesh2$n, i.e2 = 1:mesh2$n), 
+                                   list(eb.0 = rep(1, nrow(coop)), temp = rep(1, nrow(coop)), salinity = rep(1, nrow(coop)), pH = rep(1, nrow(coop)))
+                    ),
+                    tag = "ep_dat")
+stk_edna = inla.stack(e_stk, ep_stk)
+
+
+#cpue
+c_stk = inla.stack(data = list(y = cbind(NA, catch)),
+                   A = list(c_A, 1),
+                   effects = list(list(i.c = 1:mesh2$n, i.c2 = 1:mesh2$n), list(cb.0 = rep(1, length(catch)), effort = c_fish$m_effort)),
+                   tag = "c_dat")
+cp_stk = inla.stack(data = list(y = cbind(na[, 1], na[, 2])),
+                    A = list(Ap, 1),
+                    effects = list(list(i.c = 1:mesh2$n, i.c2 = 1:mesh2$n), 
+                                   list(cb.0 = rep(1, nrow(coop)), effort = rep(1, nrow(coop)))
+                    ),
+                    tag = "cp_dat")
+stk_catch = inla.stack(c_stk, cp_stk)
+
+stk = inla.stack(stk_edna, stk_catch)
+
+# formula
+formula = y ~ 0 + cb.0 + eb.0 + offset(effort) + f(temp, model = "rw1") + f(salinity, model = "rw1") + f(pH, model = "rw1") + f(i.c, model = spde) + f(i.e, copy = "i.c", fixed = FALSE) + f(i.c2, model = spde) + f(i.e2, model = spde)
+# + f(DO, model = "rw1") 
+
+
+# formula = y ~ 0 + cb.0 + eb.0 + offset(effort) + f(temp, model = "rw1") + f(salinity, model = "rw1") + f(DO, model = "rw1") + f(pH, model = "rw1") + f(i.c, model = spde) + f(i.e, copy = "i.c", fixed = FALSE) + f(i.c2, model = spde) + f(i.e2, model = spde)
+# formula = y ~ 0 + eb.0 + cb.0 + f(temp, model = "rw1") + f(salinity, model = "rw1") + f(DO, model = "rw1") + f(pH, model = "rw1") + f(i.e, model = spde) + f(i.c, copy = "i.e", fixed = FALSE) + f(effort, model = "rw1")
+
+
+# fitting
+# *** Warning *** You might want to consider to setting ``control.predictor=list(link=...)''
+# *** Warning *** otherwise the identity link will be used to compute the fitted values for NA data
+res_isi = inla(formula, 
+                data = inla.stack.data(stk), 
+                family = c("binomial", "binomial"), 
+                control.predictor = list(compute = TRUE, A = inla.stack.A(stk), link = 1), 
+                control.results = list(return.marginals.random = FALSE, return.marginals.predictor = FALSE), 
+                control.compute = list(waic = TRUE, dic = TRUE))
+# *** ERROR *** 	fail to create directory [/private/var/folders/9g/2v4y6m0x5vs6ccnjwtdf6py40000gp/T/RtmpTp9dhR/file2b33361f6d7/results.files/random.effect00000001]: No such file or directory
+# 
+# sh: line 1: 15503 Segmentation fault: 11  '/Library/Frameworks/R.framework/Versions/4.0/Resources/library/INLA/bin/mac/64bit/inla.run' -b -s -v -t 4 '/private/var/folders/9g/2v4y6m0x5vs6ccnjwtdf6py40000gp/T/RtmpTp9dhR/file2b33361f6d7/Model.ini' > '/private/var/folders/9g/2v4y6m0x5vs6ccnjwtdf6py40000gp/T/RtmpTp9dhR/file2b33361f6d7/Logfile.txt'
+# inla.inlaprogram.has.crashed() でエラー: 
+#   The inla-program exited with an error. Unless you interupted it yourself, please rerun with verbose=TRUE and check the output carefully.
+# If this does not help, please contact the developers at <help@r-inla.org>.
+
+res_mako$waic$waic; res_mako$dic$dic #929.9, 928.4
+# res_kono2$waic$waic; res_kono2$dic$dic #640.4, 640.5 
+summary(res_mako)
+
+# plot the fitted values on a map -------------------------------
+best_kono = res_mako
+
+index_ep = inla.stack.index(stk, tag = "ep_dat")$data
+index_cp = inla.stack.index(stk, tag = "cp_dat")$data
+
+pred_mean_e = best_kono$summary.fitted.values[index_ep, "mean"]
+pred_mean_c = best_kono$summary.fitted.values[index_cp, "mean"]
+pred_ll_e = best_kono$summary.fitted.values[index_ep, "0.025quant"]
+pred_ul_e = best_kono$summary.fitted.values[index_ep, "0.975quant"]
+pred_ll_c = best_kono$summary.fitted.values[index_cp, "0.025quant"]
+pred_ul_c = best_kono$summary.fitted.values[index_cp, "0.975quant"]
+
+dpm_e = rbind(data.frame(east = coop[, 1], north = coop[, 2],
+                         value = pred_mean_e, variable = "pred_mean_eDNA"),
+              data.frame(east = coop[, 1], north = coop[, 2],
+                         value = pred_ll_e, variable = "pred_ll_eDNA"),
+              data.frame(east = coop[, 1], north = coop[, 2],
+                         value = pred_ul_e, variable = "pred_ul_eDNA"))
+dpm_c = rbind(data.frame(east = coop[, 1], north = coop[, 2],
+                         value = pred_mean_c, variable = "pred_mean_catch"),
+              data.frame(east = coop[, 1], north = coop[, 2],
+                         value = pred_ll_c, variable = "pred_ll_catch"),
+              data.frame(east = coop[, 1], north = coop[, 2],
+                         value = pred_ul_c, variable = "pred_ul_catch"))
+
+dpm_e$variable = as.factor(dpm_e$variable)
+dpm_c$variable = as.factor(dpm_c$variable)
+
+g1 = ggplot(data = dpm_e, aes(east, north, fill = value))
+g2 = ggplot(data = dpm_c, aes(east, north, fill = value))
+t = geom_tile()
+f = facet_wrap(~ variable)
+c = coord_fixed(ratio = 1)
+s = scale_fill_gradient(name = "encounter prob. (logit)", low = "blue", high = "orange")
+g1+t+f+c+s+theme_bw()
+g2+t+f+c+s+theme_bw()
+
+# with map
+world_map <- map_data("world")
+jap <- subset(world_map, world_map$region == "Japan")
+jap_cog <- jap[jap$lat > 35 & jap$lat < 38 & jap$long > 139 & jap$long < 141, ]
+pol = geom_polygon(data = jap_cog, aes(x=long, y=lat, group=group), colour="gray 50", fill="gray 50")
+c_map = coord_map(xlim = c(139.5, 140.3), ylim = c(35, 35.75))
+
+dpm = rbind(dpm_e, dpm_c)
+m_dpm = dpm %>% filter(str_detect(variable, "mean"))
+unique(m_dpm$variable)
+
+# facet
+g = ggplot(data = m_dpm, aes(east, north, fill = value))
+t = geom_tile()
+f = facet_wrap(~ variable)
+c = coord_fixed(ratio = 1)
+s = scale_fill_gradient(name = "encounter prob. (logit)", low = "blue", high = "orange")
+g+t+f+c+s+pol+c_map+theme_bw()+labs(title = "konosiro")
+
+#eDNA
+g = ggplot(data = m_dpm %>% filter(variable == "pred_mean_eDNA"), aes(east, north, fill = value))
+t = geom_tile()
+c = coord_fixed(ratio = 1)
+s = scale_fill_gradient(name = "encounter prob. (logit)", low = "blue", high = "orange")
+g+t+c+s+pol+c_map+theme_bw()+labs(title = "suzuki")
+#pred_mean_catch
+g = ggplot(data = m_dpm %>% filter(variable == "pred_mean_catch"), aes(east, north, fill = value))
+t = geom_tile()
+c = coord_fixed(ratio = 1)
+s = scale_fill_gradient(name = "encounter prob. (logit)", low = "blue", high = "orange")
+g+t+c+s+pol+c_map+theme_bw()+labs(title = "suzuki")
+
+
+# projecting the spatial field ----------------------------------
+# latent distribution -------------------------------------------
+range_e = apply(mesh2$loc[, c(1, 2)], 2, range)
+# range_e = apply(coop, 2, range)
+proj_e = inla.mesh.projector(mesh2, xlim = range_e[, 1], ylim = range_e[, 2], dims = c(50, 50))
+mean_s_ie = inla.mesh.project(proj_e, best_kono$summary.random$i.e$mean) #i.c??
+sd_s_ie = inla.mesh.project(proj_e, best_kono$summary.random$i.e$sd) #i.c??
+
+df_ie = expand.grid(x = proj_e$x, y = proj_e$y)
+df_ie$mean_s = as.vector(mean_s_ie)
+df_ie$sd_s = as.vector(sd_s_ie)
+
+require(viridis)
+require(cowplot)
+require(gridExtra)
+
+g = ggplot(df_ie, aes(x = x, y = y, fill = mean_s_ie))
+r = geom_raster()
+v = scale_fill_viridis(na.value = "transparent")
+c = coord_fixed(ratio = 1)
+g+r+v+c+theme_bw()
+
+# with map
+world_map <- map_data("world")
+jap <- subset(world_map, world_map$region == "Japan")
+jap_cog <- jap[jap$lat > 35 & jap$lat < 38 & jap$long > 139 & jap$long < 141, ]
+pol = geom_polygon(data = jap_cog, aes(x=long, y=lat, group=group), colour="gray 50", fill="gray 50")
+c_map = coord_map(xlim = c(139.5, 140.3), ylim = c(35, 35.75))
+
+g1 = ggplot(df_ie, aes(x = x, y = y, fill = mean_s_ie))
+g2 = ggplot(df_ie, aes(x = x, y = y, fill = sd_s_ie))
+# r = geom_raster()
+t = geom_tile()
+v = scale_fill_viridis(na.value = "transparent")
+c = coord_fixed(ratio = 1)
+labs1 = labs(x = "Longitude", y = "Latitude", title = "Mean")
+labs2 = labs(x = "Longitude", y = "Latitude", title = "SD")
+m = g1+t+v+c+pol+c_map+labs1+theme_bw()
+sd = g2+t+v+c+pol+c_map+labs2+theme_bw()
+grid.arrange(m, sd, ncol = 2)
+
+
+# latent fisheries pattern -------------------------------------------
+range_e = apply(mesh2$loc[, c(1, 2)], 2, range)
+# range_e = apply(coop, 2, range)
+proj_e = inla.mesh.projector(mesh2, xlim = range_e[, 1], ylim = range_e[, 2], dims = c(50, 50))
+mean_s_ic2 = inla.mesh.project(proj_e, best_kono$summary.random$i.c2$mean)
+sd_s_ic2 = inla.mesh.project(proj_e, best_kono$summary.random$i.c2$sd)
+
+df_ic2 = expand.grid(x = proj_e$x, y = proj_e$y)
+df_ic2$mean_s = as.vector(mean_s_ic2)
+df_ic2$sd_s = as.vector(sd_s_ic2)
+
+g1 = ggplot(df_ic2, aes(x = x, y = y, fill = mean_s_ic2))
+g2 = ggplot(df_ic2, aes(x = x, y = y, fill = sd_s_ic2))
+# r = geom_raster()
+t = geom_tile()
+v = scale_fill_viridis(na.value = "transparent")
+c = coord_fixed(ratio = 1)
+labs1 = labs(x = "Longitude", y = "Latitude", title = "Mean")
+labs2 = labs(x = "Longitude", y = "Latitude", title = "SD")
+m = g1+t+v+c+pol+c_map+labs1+theme_bw()
+sd = g2+t+v+c+pol+c_map+labs2+theme_bw()
+grid.arrange(m, sd, ncol = 2)
+
+
+# latent pom pattern -------------------------------------------
+range_e = apply(mesh2$loc[, c(1, 2)], 2, range)
+# range_e = apply(coop, 2, range)
+proj_e = inla.mesh.projector(mesh2, xlim = range_e[, 1], ylim = range_e[, 2], dims = c(50, 50))
+mean_s_ie2 = inla.mesh.project(proj_e, best_kono$summary.random$i.e2$mean)
+sd_s_ie2 = inla.mesh.project(proj_e, best_kono$summary.random$i.e2$sd)
+
+df_ie2 = expand.grid(x = proj_e$x, y = proj_e$y)
+df_ie2$mean_s = as.vector(mean_s_ie2)
+df_ie2$sd_s = as.vector(sd_s_ie2)
+
+g1 = ggplot(df_ie2, aes(x = x, y = y, fill = mean_s_ie))
+g2 = ggplot(df_ie2, aes(x = x, y = y, fill = sd_s_ie))
+# r = geom_raster()
+t = geom_tile()
+v = scale_fill_viridis(na.value = "transparent")
+c = coord_fixed(ratio = 1)
+labs1 = labs(x = "Longitude", y = "Latitude", title = "Mean")
+labs2 = labs(x = "Longitude", y = "Latitude", title = "SD")
+m = g1+t+v+c+pol+c_map+labs1+theme_bw()
+sd = g2+t+v+c+pol+c_map+labs2+theme_bw()
+grid.arrange(m, sd, ncol = 2)
+
+
+
+# environmental effect -----------------------------------------------------------
+# plot(res_ana$summary.random$`inla.group(temp)`$ID,
+#      res_ana$summary.random$`inla.group(temp)`$mean,
+#      type = "l", ylab = "Temp effect", xlab = "Temp", 
+#      cex.naim = 1.5, cex.lab = 1.5, cex.axis = 1.5, cex = 1.5, lwd = 2)
+# plot(res_ana$summary.random$`inla.group(salinity)`$ID,
+#      res_ana$summary.random$`inla.group(salinity)`$mean,
+#      type = "l", ylab = "Sal. effect", xlab = "Salinity", 
+#      cex.naim = 1.5, cex.lab = 1.5, cex.axis = 1.5, cex = 1.5, lwd = 2)
+# plot(res_ana$summary.random$`inla.group(DO)`$ID,
+#      res_ana$summary.random$`inla.group(DO)`$mean,
+#      type = "l", ylab = "DO effect", xlab = "DO", 
+#      cex.naim = 1.5, cex.lab = 1.5, cex.axis = 1.5, cex = 1.5, lwd = 2)
+# plot(res_ana$summary.random$`inla.group(pH)`$ID,
+#      res_ana$summary.random$`inla.group(pH)`$mean,
+#      type = "l", ylab = "pH effect", xlab = "pH", 
+#      cex.naim = 1.5, cex.lab = 1.5, cex.axis = 1.5, cex = 1.5, lwd = 2)
+
+#not using inla.group
+# plot(best_kono$summary.random$temp$ID,
+#      best_kono$summary.random$temp$mean,
+#      type = "l", ylab = "Temp effect", xlab = "Temp", 
+#      cex.naim = 1.5, cex.lab = 1.5, cex.axis = 1.5, cex = 1.5, lwd = 2)
+# plot(best_kono$summary.random$salinity$ID,
+#      best_kono$summary.random$salinity$mean,
+#      type = "l", ylab = "Sal. effect", xlab = "Salinity", 
+#      cex.naim = 1.5, cex.lab = 1.5, cex.axis = 1.5, cex = 1.5, lwd = 2)
+# summary(e_fish)
+# plot(best_kono$summary.random$DO$ID,
+#      best_kono$summary.random$DO$mean,
+#      type = "l", ylab = "DO effect", xlab = "DO", 
+#      cex.naim = 1.5, cex.lab = 1.5, cex.axis = 1.5, cex = 1.5, lwd = 2)
+# plot(best_kono$summary.random$pH$ID,
+#      best_kono$summary.random$pH$mean,
+#      type = "l", ylab = "pH effect", xlab = "pH", 
+#      cex.naim = 1.5, cex.lab = 1.5, cex.axis = 1.5, cex = 1.5, lwd = 2)
+
+effect = rbind(data.frame(x = best_kono$summary.random$temp$ID, y = best_kono$summary.random$temp$mean, variable = "temp"),
+               data.frame(x = best_kono$summary.random$salinity$ID, y = best_kono$summary.random$salinity$mean, variable = "sal"),
+               #data.frame(x = best_kono$summary.random$DO$ID, y = best_kono$summary.random$DO$mean, variable = "do"),
+               data.frame(x = best_kono$summary.random$pH$ID, y = best_kono$summary.random$pH$mean, variable = "ph"))
+effect = effect %>% filter(x != 1)
+effect$variable = factor(effect$variable, levels = c("temp", "sal", "ph"))
+g = ggplot(effect, aes(x = x, y = y))
+l = geom_line()
+f = facet_wrap(~ variable, scales = "free")
+labs = labs(x = "Environmental variable", y = "Effect of environment", title = "konosiro")
+g+l+f+labs+theme_bw()
